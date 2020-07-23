a95441ee49b3f70cd71aa5fa34ab8ec79811cf4b
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-07-23 19:28:28 +0800

    Add more envs

--- config.sh
+++ config.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-export NAME=
-export LOCATION=
+export NAME=az-jaf-demo
+export LOCATION=southeastasia
 
 export RG=$NAME-rg
 export VNET=$NAME-vnet
-export NSG-$NAME-nsg
+export NSG=$NAME-nsg
+export SUBNET=$NAME-subnet
