b1eee5301fede72e9c667eddb6e5297769d9652e
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-07-27 18:39:24 +0800

    Remove color

--- az-resource-group.sh
+++ az-resource-group.sh
@@ -3,16 +3,16 @@
 source ./config.sh
 
 # Create Resource Group
-echo "${GREEN} Creating Resource Group: ${RED} ${RG}"
+echo "${GREEN} Creating Resource Group: ${RED} ${RG} ${NOCOLOR}"
 az group create -l $LOCATION -n $RG
 
 # Create VNET with subnet
-echo "${GREEN} Creating Virtual Network: ${RED} ${VNET}"
+echo "${GREEN} Creating Virtual Network: ${RED} ${VNET} $NOCOLOR"
 az network vnet create -g $RG -n $VNET --address-prefix 10.0.0.0/16 \
       --subnet-name $SUBNET-0 --subnet-prefix 10.0.0.0/24
 
 # Create NSG
-echo "${GREEN} Creating Network Security Group: ${RED} ${NSG}"
+echo "${GREEN} Creating Network Security Group: ${RED} ${NSG} ${NOCOLOR}"
 az network nsg create -g $RG \
   -n $NSG \
   --tags no_80 no_22 nsg
@@ -31,7 +31,7 @@ az network nsg rule create -g $RG \
     --priority 1000
 
 # Create ACR
-echo "${GREEN} Creating Container Registry: ${RED} ${ACR}"
+echo "${GREEN} Creating Container Registry: ${RED} ${ACR} ${NOCOLOR}"
 az acr create -n $ACR -g $RG --sku basic 
 
 # Login to ACR 
@@ -39,7 +39,7 @@ az acr login --name $ACR
 
 # Create Service Principal 
 SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --skip-assignment)
-echo "${GREEN} Creating Service Principal"
+echo "${GREEN} Creating Service Principal ${NOCOLOR}"
 echo $SERVICE_PRINCIPAL > service_principal.txt 
 
 jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' service_principal.txt > service_principal.env 
