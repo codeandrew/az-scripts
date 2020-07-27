d1d367b751e8a198cc3958a33ab9cd4e7ca324b3
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-07-27 16:43:18 +0800

    Verbosing Progress

--- az-aks.sh
+++ az-aks.sh
@@ -4,15 +4,17 @@ source ./config.sh
 source ./service_principal.env
 
 # Now we assign a role with the created service principal create before
+echo "${GREEN} Assigning Role to Service Principal"
 az role assignment create --assignee $appId --scope $ACRID --role Reader 
 
 # Create the Azure Kubernetes Cluster 
+echo "${GREEN} Creating Kubernetes Cluster: ${RED}${AKS}"
 az aks create -g $RG -n $AKS --node-count 1 \
      --service-principal $appId --client-secret $password \
-     --generate-ssh-keys 
+     --generate-ssh-keys > aks.log
+
+cat aks.log
 
 # Now we get the credentials 
-az aks install-cli # if you don't have kubectl
-AKS_CRED=$(az aks get-credentials -g $RG -n $AKS)
-
-echo $AKS_CRED > aks_credentials.txt
+echo "Saving Kubernetes Credentials to Host"
+az aks get-credentials -g $RG -n $AKS
