2225efcf1367bb70ca4e5c263550831260d12cd0
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-07-28 20:25:26 +0800

    Verbose az-vm script

--- az-vm.sh
+++ az-vm.sh
@@ -2,15 +2,19 @@
 
 source ./config.sh
 
+echo "${GREEN} Creating Public IP ${NOCOLOR}"
 az network public-ip create -n $NAME-ip-0\
   -g $RG -l $LOCATION \
   --allocation-method dynamic
 
+echo "${GREEN} Creating Network Interface: ${RED} ${NAME}-nic ${NOCOLOR}"
 az network nic create -g $RG \
   -n $NAME-nic --vnet-name $VNET \
   --subnet $SUBNET-0 \
   --public-ip-address $NAME-ip-0
 
+echo "${GREEN} Creating Virtual Machine: ${RED} ${VM} ${NOCOLOR}"
+echo "Operating System: Ubuntu"
 az vm create -n $VM -g $RG --image UbuntuLTS \
     --nics $NAME-nic \
     --vnet-name $VNET \
