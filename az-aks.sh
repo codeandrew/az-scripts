d3ae44a137bc32ec7ab085b92a1cc28eccd42fb3
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-10-21 20:11:20 +0800

    Initial AKS Scripts

--- az-aks.sh
+++ az-aks.sh
@@ -31,23 +31,23 @@ az network nsg rule create -g $RG \
     --priority 1000
 
 # Create ACR
-echo "${GREEN} Creating Container Registry: ${RED} ${ACR} ${NOCOLOR}"
-az acr create -n $ACR -g $RG --sku basic 
+# echo "${GREEN} Creating Container Registry: ${RED} ${ACR} ${NOCOLOR}"
+# az acr create -n $ACR -g $RG --sku basic 
 
 # Login to ACR 
-az acr login --name $ACR 
+# az acr login --name $ACR 
 
-# Create Service Principal 
-SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --skip-assignment)
-echo "${GREEN} Creating Service Principal ${NOCOLOR}"
-echo $SERVICE_PRINCIPAL > service_principal.txt 
+# # Create Service Principal 
+# SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --skip-assignment)
+# echo "${GREEN} Creating Service Principal ${NOCOLOR}"
+# echo $SERVICE_PRINCIPAL > service_principal.txt 
 
-jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' service_principal.txt > service_principal.env 
-echo "This is your Service Principal Get all values, Assign later"
-cat service_principal.env 
+# jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' service_principal.txt > service_principal.env 
+# echo "This is your Service Principal Get all values, Assign later"
+# cat service_principal.env 
 
-ID=$(az acr show -g $RG -n $ACR | jq .id )
-echo "ACRID=$ID" >> service_principal.env
+# ID=$(az acr show -g $RG -n $ACR | jq .id )
+# echo "ACRID=$ID" >> service_principal.env
 
 source ./service_principal.env
 
