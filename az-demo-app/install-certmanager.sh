12ca14b9a596ff0078f091c87eed0a5dc4a12899
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-10-22 12:04:16 +0800

    Create install cert manager

---
+++ az-demo-app/install-certmanager.sh
@@ -0,0 +1,17 @@
+# Label the cert-manager namespace to disable resource validation
+kubectl label namespace ingress-basic cert-manager.io/disable-validation=true
+
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
+helm install \
+  cert-manager \
+  --namespace ingress-basic \
+  --version v0.16.1 \
+  --set installCRDs=true \
+  --set nodeSelector."beta\.kubernetes\.io/os"=linux \
+  jetstack/cert-manager
