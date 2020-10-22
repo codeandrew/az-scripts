d9ea6b0df8010b5b32393c20e602f6b14d7ce5cb
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-10-22 13:40:08 +0800

    Working DNS Script

--- az-dns.sh
+++ az-dns.sh
@@ -17,3 +17,23 @@ az network dns record-set a add-record \
 az network dns record-set list \
     -g $RG \
     -z $DNS
+
+## REFERENCE
+# https://docs.microsoft.com/en-us/azure/aks/ingress-tls
+
+# Public IP address of your ingress controller
+# Get tne Public IP inside the AKS cluster RG
+# example : MC_az-jaf-demo-rg_az-jaf-demo-aks_southeastasia
+IP=20.195.48.181
+
+# Name to associate with public IP address
+DNSNAME="azjafdemo-aks"
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with DNS name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
+
+# Display the FQDN
+az network public-ip show --ids $PUBLICIPID --query "[dnsSettings.fqdn]" --output  tsv
