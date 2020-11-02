b96e1b1f54ffcde1fb0349e232ade4a7e3947d86
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-11-02 19:00:42 +0800

    Rename

---
+++ az-demo-app/scripts/ingress-setup.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+source ./config.sh 
+
+AKS_RG=MC_jafdemoproject-rg_jafdemoproject-aks_southeastasia
+
+
+# First get the resource group name of the AKS cluster
+AKS_RG=$(az aks show --resource-group $RG \
+    --name $AKS \
+    --query nodeResourceGroup \
+    -o tsv)
+
+read -p 'wait till ok' OK
+echo $OK
+
+# Create Public IP
+az network public-ip create \
+    --resource-group $AKS_RG \
+    --name $PUBIP \
+    --sku Standard \
+    --allocation-method static \
+    --query publicIp.ipAddress \
+    -o tsv
+
+# Create a namespace for your ingress resources
+kubectl create namespace ingress-basic
+
+# Add the ingress-nginx repository
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+read -p 'Your Public IP Address for Azure Portal: ' NEW_PUBLIC_IP
+read -p 'Your DNS: ' NEW_DNS
+
+# Use Helm to deploy an NGINX ingress controller
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+    --namespace ingress-basic \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set controller.service.loadBalancerIP="$NEW_PUBLIC_IP" \
+    --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"="$NEW_DNS"
+
+
+# For Checking External IP Pending
+kubectl --namespace ingress-basic get services -o wide -w nginx-ingress-ingress-nginx-controller
+
+# Check DNS Label if working
+az network public-ip list \
+    --resource-group $AKS_RG \
+    --query "[?name=='$PUBIP'].[dnsSettings.fqdn]" \
+    -o tsv
---
+++ az-demo-app/scripts/ip-dns-setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source ./config.sh
+
+# Create Public IP
+az network public-ip create \
+    --resource-group $AKS_RG \
+    --name $PUBIP \
+    --sku Standard \
+    --allocation-method static \
+    --query publicIp.ipAddress \
+    -o tsv
+
+
+# Create DNS zone
+az network dns zone create \
+    -g $RG -n $DNS
+
+# Create DNS record
+az network dns record-set a add-record \
+    -g $RG \
+    -z $DNS \
+    -n www \
+    -a 10.10.10.10
+
+# View Records
+az network dns record-set list \
+    -g $RG \
+    -z $DNS
+
+## REFERENCE
+# https://docs.microsoft.com/en-us/azure/aks/ingress-tls
+
+# Public IP address of your ingress controller
+# Get tne Public IP inside the AKS cluster RG
+# example : MC_az-jaf-demo-rg_az-jaf-demo-aks_southeastasia
+echo "What is your public IP?"
+read IP
+#IP=20.195.60.145
+
+# Name to associate with public IP address
+DNSNAME=$NAME
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with DNS name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
+
+# Display the FQDN
+az network public-ip show --ids $PUBLICIPID --query "[dnsSettings.fqdn]" --output  tsv
