986b6a845c1fa2cd5b3b7e00aa3ce21d20fb5b01
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-11-03 16:26:36 +0800

    Update tls manifest

--- az-demo-app/tls-manifest/install-certmanager.sh
+++ az-demo-app/tls-manifest/install-certmanager.sh
@@ -1,5 +1,5 @@
 # Label the cert-manager namespace to disable resource validation
-kubectl label namespace ingress-basic cert-manager.io/disable-validation=true
+kubectl label namespace cert-manager cert-manager.io/disable-validation=true
 
 # Add the Jetstack Helm repository
 helm repo add jetstack https://charts.jetstack.io
@@ -7,11 +7,18 @@ helm repo add jetstack https://charts.jetstack.io
 # Update your local Helm chart repository cache
 helm repo update
 
+# Create Namespace
+kubectl create namespace cert-manager
+
 # Install the cert-manager Helm chart
 helm install \
   cert-manager \
-  --namespace ingress-basic \
+  --namespace cert-manager \
   --version v0.16.1 \
   --set installCRDs=true \
   --set nodeSelector."beta\.kubernetes\.io/os"=linux \
   jetstack/cert-manager
+
+NS=app
+kubectl apply -f cluster-issuer.yaml -n $NS
+kubectl apply -f certificates.yaml -n $NS --validate=false
