de4ed6dba31790ca70c73716b3ca363e38fbce55
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-07-24 20:17:34 +0800

    use Managed Identity in Aks

--- aks-demo/scripts/aks-setup.sh
+++ aks-demo/scripts/aks-setup.sh
@@ -1,52 +1,40 @@
 #/bin/bash
-# version 1.0.0
+# version 1.1.0
 # using service principal 
 
 source ./config.sh
 
 # Create Resource Group
-echo "${GREEN} Creating Resource Group: ${RED} ${RG} ${NOCOLOR}"
+echo "Creating Resource Group: ${RG}"
 az group create -l $LOCATION -n $RG
 
 # Create ACR
-echo "${GREEN} Creating Container Registry: ${RED} ${ACR} ${NOCOLOR}"
+echo "Creating Container Registry: ${ACR}"
 az acr create -n $ACR -g $RG --sku standard 
 
 # Login to ACR 
 az acr login --name $ACR 
 
-# Create Service Principal 
-sp_name=sp-aks-${RG}
-SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --skip-assignment --name ${sp_name})
-echo "${GREEN} Creating Service Principal ${NOCOLOR}"
+# Get the id of the ACR
+ACRID=$(az acr show --name $ACR --query id --output tsv)
 
-SERVICE_PRINCIPAL_ID=$(echo ${SERVICE_PRINCIPAL} | jq .appId -r) 
-CLIENT_SECRET=$(echo ${SERVICE_PRINCIPAL} | jq .password -r) 
-
-ACRID=$(az acr show -g $RG -n $ACR | jq .id )
-
-# Now we assign a role with the created service principal create before
-echo "${GREEN} Assigning Role to Service Principal"
-az role assignment create --assignee $SERVICE_PRINCIPAL_ID --scope $ACRID --role Reader 
-
-# Create the Azure Kubernetes Cluster 
-echo "${GREEN} Creating Kubernetes Cluster: ${RED}${AKS}"
+# Create the Azure Kubernetes Cluster with managed identity
+echo "Creating Kubernetes Cluster: ${AKS}"
 az aks create \
-    --resource-group ${RG} \
-    --location ${LOCATION} \
-    --name ${AKS} \
-    --service-principal ${SERVICE_PRINCIPAL_ID} \
-    --client-secret ${CLIENT_SECRET} \
-    --node-count 2 \
-    --enable-addons monitoring \
-    --generate-ssh-keys \
-    --verbose
+  --resource-group ${RG} \
+  --location ${LOCATION} \
+  --name ${AKS} \
+  --node-count 2 \
+  --enable-addons monitoring \
+  --generate-ssh-keys \
+  --enable-managed-identity \
+  --verbose
     
 # Now we get the credentials 
 echo "Saving Kubernetes Credentials to Host"
 az aks get-credentials -g $RG -n $AKS
 
-# Attaching ACR in our AKS
+# Attach ACR to AKS
 echo "Attaching ACR to AKS"
 az aks update -n $AKS -g $RG --attach-acr $ACR
 
@@ -56,3 +44,4 @@ kubectl create namespace $NAMESPACE
 # Create example Application
 kubectl run test-server --image=gcr.io/google_containers/echoserver:1.8 -n $NAMESPACE
 kubectl expose pod test-server --type=NodePort --port=80 --target-port=8080 -n $NAMESPACE
+
