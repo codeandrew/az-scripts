9db926617b96b787f491af89a747bbebab437c93
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-10-05 18:20:53 +0800

    Create install-grafana.sh

---
+++ aks-demo/scripts/install-grafana.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x 
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus \
+  prometheus-community/kube-prometheus-stack \
+  --namespace monitoring \
+  --create-namespace
